18aab6a6650d35f9fab7bc59855862321ebb2f6b
Nguyễn Bá Tình <tinhnb.tech@gmail.com>
2023-03-21 16:21:54 +0700

    TinhNB commit

--- OnlineShop/OnlineShop.Web/Scripts/spa/app.js
+++ OnlineShop/OnlineShop.Web/Scripts/spa/app.js
@@ -1,11 +1,31 @@
 ﻿/// <reference path="../plugins/angular/angular.js" />
 
 var myApp = angular.module('myModule', []);
-myApp.controller('myController', myController);
+myApp.controller('schoolController', schoolController);
+myApp.service('Validator', Validator);
 
-myController.$inject = ['$scope'];
+schoolController.$inject = ['$scope', 'Validator'];
 
 //declare
-function myController($scope) {
-    $scope.message = "This is my message from controller";
+function schoolController($scope, Validator) {
+    $scope.checkNumber = function () {
+        $scope.message = Validator.checkNumber(2);
+    }
+
+    $scope.num = 1;
+}
+
+//custom service
+function Validator($window) {
+    return {
+        checkNumber: checkNumber
+    }
+
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return 'This is even';
+        } else {
+            return 'This is odd';
+        }
+    }
 }
